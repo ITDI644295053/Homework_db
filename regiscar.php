45baa7c41690da4cc3dc42be948d3f00652eb68b
NASREEN ^^ <99543021+ITDI644295053@users.noreply.github.com>
2023-03-13 21:59:00 +0700

    Update regiscar.php

--- regiscar.php
+++ regiscar.php
@@ -2,10 +2,9 @@
 $servername = "localhost";
 $dbusername = "root";
 $dbpassword = "";
-$dbname = "carshop";
-
+$dbname="carshop";
 // Create connection
-$conn = mysqli_connect($servername, $dbusername, $dbpassword, $dbname);
+$conn = mysqli_connect($servername, $dbusername, $dbpassword,$dbname);//คำสั่ง connect database
 
 // Check connection
 if (!$conn) {
@@ -14,38 +13,79 @@ if (!$conn) {
 //echo "Connected successfully";
 ?>
 
+
+
 <!DOCTYPE html>
 <html lang="en">
     <head>
         <meta charset="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <link href='https://fonts.googleapis.com/css?family=Prompt' rel='stylesheet'>
-            <style>
-                body {                        
-                    font-family: 'Prompt';
-                    background:#D8BFD8;
-                    position: fixed;  
-                    top: 50%;  
-                    left: 50%;  
-                    transform: translate(-50%, -50%);
-                }
-                form {  
-                    border:8px solid #DA70D6;
-                    padding:20px 50px; 
-                    background:#E6E6FA;
-                    width:50%;
-                    border-radius:45px;}
-                </style>
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Car Shop</title>
+        <!-- Favicon-->
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="css/styles.css" rel="stylesheet" />
     </head>
     <body>
-        <form action="processcar.php" method="post" enctype = "multipart/form-data">
-            <center><h2>Car Shop</h2></center>
-            brand : <input type="text" name="brand" ><br><br>         
-            color : <input type="text" name="color" ><br><br> 
-            vehicle registration : <input type="text" name="regis" ><br><br>
-            price : <input type="number" name="price" ><br><br>
-            photo : <input type="file" value="Upload" name="photo" ><br><br>
-            <center><input type="submit" value="Save"></center>
-        </form>
+        <div class="d-flex" id="wrapper">
+            <!-- Sidebar-->
+            <div class="border-end bg-white" id="sidebar-wrapper">
+                <div class="sidebar-heading border-bottom bg-light">Car Shop</div>
+                <div class="list-group list-group-flush">
+                    <a class="list-group-item list-group-item-action list-group-item-light p-3" href="showcar.php">Car</a>
+                    <a class="list-group-item list-group-item-action list-group-item-light p-3" href="#!">Add Car</a>
+                    <a class="list-group-item list-group-item-action list-group-item-light p-3" href="#!">Expense</a>
+                    <a class="list-group-item list-group-item-action list-group-item-light p-3" href="#!">Setting</a>
+                </div>
+            </div>
+            <!-- Page content wrapper-->
+            <div id="page-content-wrapper">
+                <!-- Top navigation-->
+                <nav class="navbar navbar-expand-lg navbar-light bg-light border-bottom">
+                    <div class="container-fluid">
+                        <button class="btn btn-primary" id="sidebarToggle">Menu</button>
+                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                            <ul class="navbar-nav ms-auto mt-2 mt-lg-0">
+                                <li class="nav-item active"><a class="nav-link" href="#!">Home</a></li>
+                                <li class="nav-item"><a class="nav-link" href="#!">Link</a></li>
+                                <li class="nav-item dropdown">
+                                    <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
+                                    <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                                        <a class="dropdown-item" href="#!">Action</a>
+                                        <a class="dropdown-item" href="#!">Another action</a>
+                                        <div class="dropdown-divider"></div>
+                                        <a class="dropdown-item" href="#!">Something else here</a>
+                                    </div>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </nav>
+                <!-- Page content-->
+                <div class="container-fluid">
+                    <h1 class="mt-4">Add Car</h1>
+                     
+                    <form action="processcar.php" method="post" enctype = "multipart/form-data">
+                        brand : <input type="text" name="brand" ><br><br>         
+                        color : <input type="text" name="color" ><br><br> 
+                        vehicle registration : <input type="text" name="regis" ><br><br>
+                        price : <input type="number" name="price" ><br><br>
+                        photo : <input type="file" value="Upload" name="photo" ><br><br>
+                        <input type="submit" value="Add">
+                    </form>
+                    
+                </div>
+            </div>
+        </div>
+        <!-- Bootstrap core JS-->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+        
     </body>
 </html>
+
+
